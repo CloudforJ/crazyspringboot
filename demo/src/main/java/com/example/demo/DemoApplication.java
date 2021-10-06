19141093b92e70900d366e06ad9b081e345562fd
CloudforJ <bowenlu.cloud@gmail.com>
2021-10-06 16:51:40 +0800

    This is a demo spring boot app

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	@ResponseBody
+	public String index() {
+		return "This is the default page";
+	}
+
+	@GetMapping("/rest")
+	@ResponseBody
+	public String rest() {
+		return "This is not the default page";
+	}
+
+
+}
